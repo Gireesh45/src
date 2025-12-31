9588476e7acce365517b4e962fda21971bf5532f
Gireesh Kumar <141120789+Gireesh45@users.noreply.github.com>
2025-12-31 23:07:23 +0530

    Disable routing configuration in vite.config.js
    Comment out routing configuration in Vite config.

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  "routes": [
-    {
-      "src": "/(.*)",
-      "dest": "/index.html"
-    }
-  ]
+  // "routes": [
+  //   {
+  //     "src": "/(.*)",
+  //     "dest": "/index.html"
+  //   }
+  // ]
 })
