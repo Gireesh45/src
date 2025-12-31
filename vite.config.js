cc3477412ed615887c8311cb988ac3868eb7f258
Gireesh Kumar <141120789+Gireesh45@users.noreply.github.com>
2025-12-31 23:08:40 +0530

    Remove commented routing configuration
    Removed commented-out code related to routing.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,1 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  // "routes": [
-  //   {
-  //     "src": "/(.*)",
-  //     "dest": "/index.html"
-  //   }
-  // ]
-})
